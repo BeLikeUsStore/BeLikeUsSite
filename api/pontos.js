4293bf97a580429af9c2aff2f803fa3543ce575f
BeLikeUsStore <belikeusstore@gmail.com>
2026-01-24 18:06:37 -0300

    fix: api 2.0

--- api/pontos.js
+++ api/pontos.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.VITE_SUPABASE_ANON_KEY
 );
 
 module.exports = async function handler(req, res) {
